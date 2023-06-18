df62f6831144e19fd6dce42f69843016271cbd27
freekos <maks.mafia00@mail.ru>
2023-06-18 22:45:00 +0600

    feat: add routes and user-route

---
+++ cmd/app/routes.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"golang-postgres/internal/handler"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) Routes() http.Handler {
+	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	r.Use(middleware.Heartbeat("/ping"))
+
+	handler := handler.NewHandler(&app.repo)
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/users", handler.User.Handler)
+	})
+
+	return r
+}
